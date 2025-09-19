d28c3d900c66a1a7b3a1abb51df07f2e77996eb6
GerBaldus <gerard_baldus@hotmail.com>
2025-09-19 11:32:23 -0300

    Animaciones

--- js/carrito.js
+++ js/carrito.js
@@ -1,11 +1,23 @@
 const listaCarrito = document.getElementById("listaCarrito");
 const formulario = document.getElementById("formulario");
+const botonModo = document.getElementById("modo")
 function traerLocal(c) {
   try {
     return JSON.parse(localStorage.getItem(c)) || null;
   } catch (e) {
     return null;
   }
+}
+function cargarTema() {
+  const t = localStorage.getItem("tema");
+  if (t) document.documentElement.setAttribute("data-tema", t);
+}
+function cambiarTema() {
+  const actual = document.documentElement.getAttribute("data-tema");
+  const nuevo = actual === "oscuro" ? "" : "oscuro";
+  if (nuevo) document.documentElement.setAttribute("data-tema", nuevo);
+  else document.documentElement.removeAttribute("data-tema");
+  localStorage.setItem("tema", nuevo);
 }
 function guardarLocal(c, v) {
   localStorage.setItem(c, JSON.stringify(v));
@@ -26,7 +38,20 @@ async function cargarCarrito() {
   items.forEach((it) => {
     const el = document.createElement("div");
     el.className = "tarjeta";
-    el.innerHTML = `<h4>${it.nombre}</h4><p>$${it.precio} x ${it.cant}</p><div class="botones"><button data-id="${it.id}" class="boton">-</button><button data-id="${it.id}" class="boton">+</button></div>`;
+    el.innerHTML = `
+    <img src="${it.imagen}" alt="${it.nombre}" style="width:100px; height:100px; object-fit:cover; border-radius:6px;">
+    <h4>${it.nombre}</h4>
+    <p>$${it.precio} x ${it.cant}</p>
+    <div class="botones">
+    <button data-id="${it.id}" class="boton">-</button>
+    <button data-id="${it.id}" class="boton">+</button>
+    </div>
+    `;
+    const total = items.reduce((acc, it) => acc + it.precio * it.cant, 0);
+
+    document.getElementById("totalCarrito").innerHTML = `
+    <h3>Total de la compra: $${total}</h3>
+    `;
     listaCarrito.appendChild(el);
   });
 }
@@ -68,4 +93,6 @@ formulario.addEventListener("submit", async (e) => {
     alert("Error al enviar");
   }
 });
+cargarTema();
+botonModo.addEventListener("click", cambiarTema)
 cargarCarrito();
